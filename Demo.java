0b322744c143753dc457b666927f81c44106d292
ivanmijacika <imijacika20@stuy.edu>
2020-10-22 00:32:49 -0400

    I have tested hw 9 and hw 10 and they work

--- Demo.java
+++ Demo.java
@@ -2,9 +2,9 @@ public class Demo{
     public static void main(String[] args) {
         if (args.length!=0) printLoop(Integer.parseInt(args[0]));
         else printLoop(5);
-	//int[][] a = new int[][]{{4,5,9},{1,2,5},{2,6,3}};
-        //System.out.println(arrDeepToString(a).replace("},","},\n"));
-	//System.out.println(arrDeepToString(create2DArray(2,2,5)));
+	//int[][] a = new int[][]{{},{},{}};
+        //System.out.println(arrayDeepToString(a).replace("},","},\n"));
+	//System.out.println(arrayDeepToString(create2DArrayRandomized(4,4,5)));
     }
     public static void printLoop(int n){
         for(int i=1; i<=n; i++){
@@ -24,34 +24,36 @@ public class Demo{
         s=s.substring(0,s.length()-2)+"}";
         return s;
     }
-    public static String arrDeepToString(int[][] arr){
-        String s="{";
+
+
+    public static String arrayDeepToString(int[][]arr){
         if (arr.length==0) return "{}";
-        for (int i=0; i<arr.length; i++){
-            String s1="{";
-            for (int j=0; j<arr[i].length; j++){
-                s1+=String.valueOf(arr[i][j])+", ";
-            }
-            s1=s1.substring(0,s1.length()-2)+"}, ";
-            s+=s1;
+        String s="{";
+        for (int i=0; i<arr.length-1; i++){
+            s+= arrToString(arr[i])+", ";
         }
-        s=s.substring(0,s.length()-2)+"}";
-        return s;
+        return s+arrToString(arr[arr.length-1])+"}";
     }
+
 
     public static int[][] create2DArray(int rows, int cols, int maxValue){
         int[][]arr =new int[rows][cols];
-        int n=0;
         for(int i=0; i<rows; i++){
             for(int j=0; j<cols; j++){
                 arr[i][j]=(int)(Math.random()*(maxValue+1));
-                n++;
-                if(n==maxValue+1){
-                    j=cols;
-                    n=0;
-                }
             }
         }
+        return arr;
+    }
+    public static int[][] create2DArrayRandomized(int rows, int cols, int maxValue){
+        int[][]arr =new int[rows][];
+        for (int i=0; i<rows; i++){
+	    int cols1=(int)(Math.random()*(maxValue+1));
+	    arr[i]=new int[cols1];
+	    for (int j=0; j<cols1; j++){
+                arr[i][j]=(int)(Math.random()*(maxValue+1));
+            }
+	}
         return arr;
     }
 }
